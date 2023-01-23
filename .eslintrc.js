2a3712efaf40377a4f512b8cb5239b206a480ac1
Momoh Nobert <okhai0112@gmail.com>
2023-01-23 21:28:54 +0100

    added initial boilerplate & three.js dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:testing-library/react',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
+        },
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
