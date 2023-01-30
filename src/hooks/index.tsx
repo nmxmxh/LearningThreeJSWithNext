850073b779f87735b8cdec261c8058b5ccc35ae4
Momoh Nobert <okhai0112@gmail.com>
2023-01-31 00:08:55 +0100

    created toggle full screen hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,6 @@
 import useMousePosition from "./useMousePosition";
+import useToggleFullscreen from "./useToggleFullscreen";
 import useWindowAndDocument from "./useWindowAndDocument";
 import useWindowDimensions from "./useWindowDimensions";
 
-export { useMousePosition, useWindowAndDocument, useWindowDimensions };
+export { useMousePosition, useWindowAndDocument, useWindowDimensions, useToggleFullscreen };
---
+++ src/hooks/useToggleFullscreen.tsx
@@ -0,0 +1,40 @@
+import useWindowAndDocument from "./useWindowAndDocument";
+
+interface FullscreenDocument extends Document {
+  mozFullScreenElement?: Element | null;
+  webkitFullscreenElement?: Element | null;
+  msFullscreenElement?: Element | null;
+
+  mozCancelFullScreen?: Promise<void>;
+  webkitExitFullscreen?: Promise<void>;
+  msExitFullscreen?: Promise<void>;
+}
+
+interface DocumentElement extends HTMLElement {
+  mozRequestFullScreen?: Promise<void>;
+  webkitRequestFullScreen?: Promise<void>;
+  msRequestFullscreen?: Promise<void>;
+}
+
+export default function useToggleFullScreen() {
+  const { _window } = useWindowAndDocument();
+
+  function toggleFullscreen() {
+    if (_window) {
+      const doc: FullscreenDocument = window.document;
+      const docEl: DocumentElement = doc.documentElement;
+
+      const requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+      const cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+      if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+      }
+      else {
+        cancelFullScreen.call(doc);
+      }
+    }
+  }
+
+  return toggleFullscreen
+}
